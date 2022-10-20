de6bccb7c3f1418d419ea0c70b58cd6127f089ee
Tobias <tobiascm87@gmail.com>
2022-10-19 21:44:13 -0300

    Semana 3 - CRUD Cadastro de Vaga parte 1

---
+++ Estacionamento/src/model/bean/Vaga.java
@@ -0,0 +1,42 @@
+package model.bean;
+
+public class Vaga {
+
+    private int idVaga;
+    private int numero;
+    private String rua;
+    private boolean obliqua;
+
+    public int getIdVaga() {
+        return idVaga;
+    }
+
+    public void setIdVaga(int idVaga) {
+        this.idVaga = idVaga;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public boolean getObliqua() {
+        return obliqua;
+    }
+
+    public void setObliqua(boolean obliqua) {
+        this.obliqua = obliqua;
+    }
+
+}
