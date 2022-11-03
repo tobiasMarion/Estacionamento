39ca512909ca214c87338cf0db3425f15ec05d23
Tobias <>
2022-11-03 16:48:38 -0600

    Atividade Semana 5 e 6 - Listagem e Exclusão de Motorista

--- Estacionamento/src/model/dao/MotoristaDAO.java
+++ Estacionamento/src/model/dao/MotoristaDAO.java
@@ -7,6 +7,10 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.ArrayList;
+
 public class MotoristaDAO {
 
     public void create(Motorista m) {
@@ -31,6 +35,55 @@ public class MotoristaDAO {
 
         } finally {
             ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+
+    public List<Motorista> read() {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Motorista> motoristas = new ArrayList<>();
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM motorista;");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Motorista m = new Motorista();
+                m.setIdMotorista(rs.getInt("idMotorista"));
+                m.setNome(rs.getString("nome"));
+                m.setHomem(rs.getBoolean("homem"));
+                m.setRg(rs.getString("rg"));
+                m.setCpf(rs.getString("cpf"));
+                m.setCelular(rs.getString("celular"));
+                m.setEmail(rs.getString("email"));
+
+                motoristas.add(m);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar os dados: ", e);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+
+        return motoristas;
+    }
+
+    public void delete(Motorista m) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("DELETE FROM motorista WHERE idMotorista=?");
+            stmt.setInt(1, m.getIdMotorista());
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Motorista excluído com sucesso!");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir: " + e);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt);
+
         }
     }
 }
