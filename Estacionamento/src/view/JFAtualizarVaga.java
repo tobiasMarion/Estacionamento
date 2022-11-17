b857e206ab6404de62cbca4d84ed2ef1d3f41ff8
Tobias <>
2022-11-17 19:24:46 -0300

    Semana 8 - Atualizar Vaga Parte 2

--- Estacionamento/src/view/JFAtualizarVaga.java
+++ Estacionamento/src/view/JFAtualizarVaga.java
@@ -5,6 +5,8 @@ import model.dao.VagaDAO;
 
 public class JFAtualizarVaga extends javax.swing.JFrame {
 
+    private static int idVaga;
+
     /**
      * Creates new form JFAtualizarVaga
      */
@@ -183,7 +185,20 @@ public class JFAtualizarVaga extends javax.swing.JFrame {
     }//GEN-LAST:event_jBtnLimparActionPerformed
 
     private void jBtnSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnSalvarActionPerformed
+        Vaga v = new Vaga();
+        VagaDAO vdao = new VagaDAO();
 
+        v.setIdVaga(Integer.parseInt(lblIdVaga.getText()));
+        v.setNumero(Integer.parseInt(jTFNumero.getText()));
+        v.setRua(jTFRua.getText());
+
+        if (jRBObliqua.isSelected()) {
+            v.setObliqua(true);
+        } else if (jRBParalela.isSelected()) {
+            v.setObliqua(false);
+        }
+        
+        vdao.update(v);
     }//GEN-LAST:event_jBtnSalvarActionPerformed
 
     /**
@@ -216,7 +231,8 @@ public class JFAtualizarVaga extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new JFAtualizarVaga().setVisible(true);
+                JFAtualizarVaga frame = new JFAtualizarVaga(idVaga);
+                frame.setVisible(true);
             }
         });
     }
--- Estacionamento/src/view/JFListarVagas.java
+++ Estacionamento/src/view/JFListarVagas.java
@@ -78,6 +78,11 @@ public class JFListarVagas extends javax.swing.JFrame {
         });
 
         jBtnEditar.setText("Editar Vaga");
+        jBtnEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBtnEditarActionPerformed(evt);
+            }
+        });
 
         jBtnExcluir.setText("Excluir Vaga");
         jBtnExcluir.addActionListener(new java.awt.event.ActionListener() {
@@ -159,6 +164,19 @@ public class JFListarVagas extends javax.swing.JFrame {
         }
         readJTable();
     }//GEN-LAST:event_jBtnExcluirActionPerformed
+
+    private void jBtnEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnEditarActionPerformed
+        // TODO add your handling code here:
+        if (jTVaga.getSelectedRow() != -1) {
+            int vagaSelecionada = (int)jTVaga.getValueAt(jTVaga.getSelectedRow(), 0);
+            
+            JFAtualizarVaga av = new JFAtualizarVaga(vagaSelecionada);
+            av.setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(null, "Selecione uma vaga.", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+        readJTable();
+    }//GEN-LAST:event_jBtnEditarActionPerformed
 
     public void readJTable() {
         DefaultTableModel modelo = (DefaultTableModel) jTVaga.getModel();
